59e5ee53e796f786bf02afb3f277bc0a204ddbd8
jayson-ngo <nam.ngo.gtsvietnam@gmail.com>
2025-05-09 10:45:38 +0700

    feature: filter product

--- app/admin/products/list-products.tsx
+++ app/admin/products/list-products.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Plus } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { serviceColumns } from '@/app/admin/products/_components/columns';
 import { DataTable } from '@/app/admin/products/_components/service-table';
 import { AppBreadcrumb } from '@/components/app-breadcrumb';
@@ -9,22 +9,35 @@ import { Button } from '@/components/ui/button';
 import { AppRouter } from '@/src/constants/constant';
 import { useProductCollectionQuery } from '@/src/graphql/queries/productCollection.generated';
 import { PageMeta, ProductEntity } from '@/src/graphql/type.interface';
+import { ProductFormFilter } from './_components/product-filter-form';
 
 export const ListProducts = () => {
   const router = useRouter();
 
   const [page, setPage] = useState(1);
+  const [search, setSearch] = useState<string | null>('');
   const { data } = useProductCollectionQuery({
     variables: {
       paginationArgs: {
         limit: 10,
         page: page,
+        search,
       },
     },
   });
 
   const products = data?.productCollection?.items ?? [];
   const pageMeta = data?.productCollection?.meta;
+
+  const handleFilter = useCallback((values: { search?: string }) => {
+    setSearch(values?.search ?? '');
+    setPage(1);
+  }, []);
+
+  const handleRemoveFilter = useCallback(() => {
+    setSearch('');
+    setPage(1);
+  }, []);
 
   return (
     <div>
@@ -46,6 +59,7 @@ export const ListProducts = () => {
         }
       />
       <div className='p-5 bg-[#F9F9F9]'>
+        <ProductFormFilter onFilterAction={handleFilter} onRemoveFilterAction={handleRemoveFilter} />
         <div className='p-5 bg-white'>
           <p className='font-semibold text-[#202C38] mt-0 mb-5'>{pageMeta?.totalItem} phụ tùng</p>
           <DataTable
